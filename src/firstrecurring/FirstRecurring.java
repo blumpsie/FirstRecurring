7917ff4dae3faf5c206dcc69f20f04ca4909313d
Mark Erickson <blumpsie@gmail.com>
2017-10-31 10:49:28 -0400

    Main Method
    Main method is built

--- src/firstrecurring/FirstRecurring.java
+++ src/firstrecurring/FirstRecurring.java
@@ -1,21 +1,58 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This program is to see the the difference in the algorithims for
+ * finding the first recurring character in a string
  */
+
 package firstrecurring;
+
+import java.util.Scanner;
 
 /**
  *
- * @author AestheticDeath
+ * @author Mark Erickson
  */
+
 public class FirstRecurring {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner keyboard = new Scanner(System.in);
+        String testString = "";
+        long startTime = 0;
+        long endTime = 0;
+        long totalTimeNiave = 0;
+        long totalTimeBetter = 0;
+        
+        // Get user supplied string
+        System.out.println("Please enter a string?");
+        testString = keyboard.nextLine();
+       
+        startTime = System.nanoTime();
+        //firstRecurringNiave(testString);
+        endTime = System.nanoTime();
+        totalTimeNiave = endTime - startTime;
+        System.out.println("The niave method took " + totalTimeNiave
+                            + " nanoseconds");
+        
+        
+        startTime = System.nanoTime();
+        //firstRecurringBetter(testString);
+        endTime = System.nanoTime();
+        totalTimeBetter = endTime - startTime;
+        System.out.println("The niave method took " + totalTimeBetter
+                            + " nanoseconds");
+        
+        // Report which is better and by how much
+        if (totalTimeBetter < totalTimeNiave)
+        {
+            System.out.println("The second method was better by "
+                                + (totalTimeNiave - totalTimeBetter));
+        }
+        else
+        {
+            System.out.println("The first method was better by "
+                                + (totalTimeBetter - totalTimeNiave));
+        }
     }
     
 }
